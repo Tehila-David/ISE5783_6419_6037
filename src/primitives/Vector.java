818132bbc0e6793bc59d5d03dca9c84a9c21a2dc
Tehila David <tedavid@g.jct.ac.il>
2023-02-28 21:49:58 +0200

    test push...

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -3,49 +3,43 @@ public class Vector extends Point {
 
     public Vector(double x, double y, double z) {
         super(x, y, z);
-        if(xyz.equals(Double3.ZERO))
+        if (xyz.equals(Double3.ZERO))
             throw new IllegalArgumentException("vector cannot be zero");
     }
 
     Vector(Double3 vec) {
-        this(vec.d1,vec.d2, vec.d3);
+        this(vec.d1, vec.d2, vec.d3);
     }
 
-    public Vector add(Vector vec)
-    {
+    public Vector add(Vector vec) {
         return new Vector(vec.xyz.add(this.xyz));
     }
-    public Vector scale(double num)
-    {
+
+    public Vector scale(double num) {
         return new Vector(xyz.scale(num));
     }
 
-    public double dotProduct(Vector vec)
-    {
+    public double dotProduct(Vector vec) {
         return vec.xyz.d1 * xyz.d1 + vec.xyz.d2 * xyz.d2 + vec.xyz.d3 * xyz.d3;
     }
 
-    public Vector crossProduct(Vector vec)
-    {
-        double x=xyz.d2*vec.xyz.d3 -xyz.d3*vec.xyz.d2;
-        double y=xyz.d3*vec.xyz.d1 -xyz.d1*vec.xyz.d3;
-        double z=xyz.d1*vec.xyz.d2 -xyz.d2*vec.xyz.d1;
-        return new Vector(x,y,z);
+    public Vector crossProduct(Vector vec) {
+        double x = xyz.d2 * vec.xyz.d3 - xyz.d3 * vec.xyz.d2;
+        double y = xyz.d3 * vec.xyz.d1 - xyz.d1 * vec.xyz.d3;
+        double z = xyz.d1 * vec.xyz.d2 - xyz.d2 * vec.xyz.d1;
+        return new Vector(x, y, z);
     }
 
-    public double lengthSquared()
-    {
+    public double lengthSquared() {
         return dotProduct(this);
     }
 
-    public double length()
-    {
+    public double length() {
         return Math.sqrt(this.lengthSquared());
     }
 
-    public Vector normalize()
-    {
-        return new Vector(this.xyz.d1/this.length(),this.xyz.d2/this.length(),this.xyz.d3/this.length());
+    public Vector normalize() {
+        return new Vector(this.xyz.d1 / this.length(), this.xyz.d2 / this.length(), this.xyz.d3 / this.length());
     }
 
     @Override
@@ -53,8 +47,10 @@ public class Vector extends Point {
         return super.toString();
 
     }
+
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
     }
+}
 
