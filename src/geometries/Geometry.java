3cc491b03aef944132f2223e6ef6fc130ae9a683
SivanLevi100 <sivanlevi94@gmail.com>
2023-02-28 21:43:47 +0200

    finish

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -1,2 +1,8 @@
-package geometries;public interface Geometry {
+package geometries;
+
+import primitives.Point;
+import primitives.Vector;
+
+public interface Geometry {
+    public Vector getNormal(Point point);
 }
--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -1,2 +1,27 @@
-package geometries;public class Plane {
+package geometries;
+
+import primitives.Point;
+import primitives.Vector;
+
+public class Plane {
+
+    final Point q0;
+    final Vector normal;
+
+    public Plane(Point q0, Vector normal) {
+        this.q0 = q0;
+        this.normal = normal.normalize();
+    }
+    public Plane(Point p1,Point p2,Point p3) {
+        normal=null;
+        q0=p1;
+    }
+
+    public Point getQ0() {
+        return q0;
+    }
+
+    public Vector getNormal() {
+        return normal;
+    }
 }
