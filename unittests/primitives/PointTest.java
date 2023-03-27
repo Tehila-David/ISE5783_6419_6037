1356f39c385e90369eb5670fd7a6b2af9cfff809
Tehila David <tedavid@g.jct.ac.il>
2023-03-27 09:07:02 +0300

    Adding unittests

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,28 @@
+
+package primitives;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Unit tests for primitives.Point class
+ */
+public class PointTest {
+
+
+    @org.junit.jupiter.api.Test
+    void add()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void distanceSquared() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void distance() {
+    }
+}
