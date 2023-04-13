2ffd19db57dff3523fa99f02b31821622cc55c9f
Tehila David <tedavid@g.jct.ac.il>
2023-04-13 20:55:17 +0300

    tests of point and vector

--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -4,24 +4,101 @@ package primitives;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Unit tests for primitives.Point class
  */
 public class PointTest {
 
+    /**
+     * Test the add() method of the Point class.
+     * Equivalence Partitions:
+     * - Adding a vector to a point
+     *
+     * Boundary Values Tests:
+     * - Adding a negative vector to a point should throw an IllegalArgumentException.
+     */
     @Test
-    void testAdd() {
+    void testAdd()
+    {
+        // ============ Equivalence Partitions Tests ==============
+        Point p1=new Point(1,2,3);
+        Point p2 =new Point(1,3,4);
+        assertEquals(p2, p1.add(new Vector(0, 1, 1)));
+
+        // =============== Boundary Values Tests ==================
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> p1.add(new Vector(-1, -2, -3)),"ERROR: point + -itself does not throw an exception"
+        );
     }
 
+    /**
+     * Test the subtract() method of the Point class.
+     * Equivalence Partitions:
+     * - Subtracting a vector from a point
+     *
+     * Boundary Values Tests:
+     * - Subtracting the point from itself should throw an IllegalArgumentException.
+     */
     @Test
-    void testSubtract() {
+    void testSubtract()
+    {
+        // ============ Equivalence Partitions Tests ==============
+        Point p1=new Point(1,2,3);
+        Point p2 =new Point(1,0,0);
+        assertEquals(p2, p1.subtract(new Vector(0, 2, 3)));
+
+        // =============== Boundary Values Tests ==================
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> p1.subtract(new Vector(1, 2, 3)),"ERROR: point -itself does not throw an exception"
+        );
     }
 
+    /**
+     * Test the distanceSquared() method of the Point class.
+     * Equivalence Partitions:
+     * - Distance squared between two points
+     *
+     * Boundary Values Tests:
+     * - Calculating the distance squared between a point and itself should throw an IllegalArgumentException.
+     */
     @Test
-    void testDistanceSquared() {
+    void testDistanceSquared()
+    {
+        // ============ Equivalence Partitions Tests ==============
+        Point p1=new Point(1,2,3);
+        assertEquals(2,p1.distanceSquared(new Point(1,3,4)));
+
+        // =============== Boundary Values Tests ==================
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> p1.distanceSquared(new Point(1, 2, 3)),"ERROR: point -itself does not throw an exception"
+        );
     }
 
+    /**
+     * Test the distance() method of the Point class.
+     * Equivalence Partitions:
+     * - Distance between two points
+     *
+     * Boundary Values Tests:
+     * - Calculating the distance between a point and itself should throw an IllegalArgumentException.
+     */
     @Test
-    void testDistance() {
+    void testDistance()
+    {
+        // ============ Equivalence Partitions Tests ==============
+        Point p1=new Point(1,2,0);
+        assertEquals(1,p1.distanceSquared(new Point(1,3,0)));
+
+        // =============== Boundary Values Tests ==================
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> p1.distanceSquared(new Point(1, 2, 0)),"ERROR: point -itself does not throw an exception"
+        );
     }
+
 }
--- unittests/primitives/VectorTest.java
+++ unittests/primitives/VectorTest.java
@@ -29,41 +29,73 @@ class VectorTest {
     }
 
     @Test
-    void testScale() {
+    void testScale()
+    {
+        // ============ Equivalence Partitions Tests ==============
+        Vector v1 = new Vector(1, 2, 3);
+        assertEquals(new Vector(2, 4, 6), v1.scale(2));
+        assertEquals(new Vector(0, 0, 0), v1.scale(0));
 
+        // =============== Boundary Values Tests ==================
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> v1.scale(-1),
+                "ERROR: Scaling by a negative factor does not throw an exception"
+        );
     }
 
     @Test
-    void testDotProduct() {
-
+    void testDotProduct()
+    {
+        // ============ Equivalence Partitions Tests ==============
         Vector v1 = new Vector(1, 0, -1);
         Vector v2 = new Vector(1,1,1);
-        assertEquals(0,v1.dotProduct(v2),"ERROR: dotProduct() for orthogonal vectors is not zero");
+        assertEquals(0, v1.dotProduct(v2),"ERROR: dotProduct() for orthogonal vectors is not zero");
+        assertEquals(3, v1.dotProduct(v1), "ERROR: dotProduct() for identical vectors is not the square of the length");
 
-       // assertEquals (28,(v1.dotProduct(v2) + 28),);
-
+        // =============== Boundary Values Tests ==================
+        Vector v3 = new Vector(0, 0, 0);
+        assertEquals(0, v1.dotProduct(v3), "ERROR: dotProduct() with zero vector is not zero");
     }
 
+
+
     @Test
-    void testCrossProduct() {
+    void testCrossProduct()
+    {
+        // ============ Equivalence Partitions Tests ==============
+        Vector v1 = new Vector(1, 0, 0);
+        Vector v2 = new Vector(0,1,0);
+        assertEquals(new Vector(0, 0, 1), v1.crossProduct(v2));
+
+        // =============== Boundary Values Tests ==================
+        Vector v3 = new Vector(0, 0, 0);
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> v1.crossProduct(v3),
+                "ERROR: crossProduct() with zero vector does not throw an exception"
+        );
     }
 
     @Test
     void testLengthSquared() {
+        // ============ Equivalence Partitions Tests ==============
         Vector v1 = new Vector(1, 2, 3);
         assertEquals(14, v1.lengthSquared(), 0.00001, "ERROR: lengthSquared() wrong value");
-
+        assertEquals(0, new Vector(0, 0, 0).lengthSquared(), "ERROR: lengthSquared() with zero vector is not zero");
     }
 
     @Test
     void testLength() {
+// =    =========== Equivalence Partitions Tests ==============
         Vector v1 = new Vector(1, 2, 3);
-        assertEquals(3.7416, v1.length(), 0.001, "ERROR: lengthSquared() wrong value");
-
+        assertEquals(3.7416, v1.length(), 0.001, "ERROR: length() wrong value");
+        assertEquals(0, new Vector(0, 0, 0).length(), "ERROR: length() with zero vector is not zero");
     }
 
     @Test
-    void testNormalize() {
+    void testNormalize()
+    {
     }
     ////
 }
